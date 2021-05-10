000e00b4090b46b7d127d7515c79edb09d3025e7
yuzu762 <76766143+yuzu762@users.noreply.github.com>
2021-05-10 13:10:55 +0800

    Update stop.sh

--- process_monitor/stop.sh
+++ process_monitor/stop.sh
@@ -8,7 +8,7 @@ function KILL
      while true
      do
          let flag=0
-         PID=`ps -ef | grep $1 | grep -v grep | awk '{print $2 "," $8}'`
+         PID=`ps -ef | grep $1 | grep -v grep|grep -v $0| awk '{print $2 "," $8}'`
          for pid in ${PID};
          do
             APP_PID=`echo $pid | awk -F ',' '{print $1}'`
@@ -17,8 +17,8 @@ function KILL
             if [ $APP_NAME == $1 ]
             then
               let flag=1
-              ps -ef | grep $1 | grep -v grep |awk '{print "kill "  $3}'| sh
-              ps -ef | grep $1 | grep -v grep |awk '{print "kill "  $2}'| sh
+              ps -f -p$APP_PID | grep $1 | grep -v grep |awk '{print "kill "  $3}'| sh
+              ps -f -p$APP_PID | grep $1 | grep -v grep |awk '{print "kill "  $2}'| sh
             fi
          done
 
